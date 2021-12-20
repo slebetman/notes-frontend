06a547b19de4c6fd89cbc9d0a9790d305b01ce65
slebetman <slebetman@gmail.com>
2021-12-20 13:52:13 +0800

    Change color pallete

--- src/components/Note.js
+++ src/components/Note.js
@@ -41,6 +41,13 @@ function Notes () {
 				<CompactPicker
 					color={color}
 					onChangeComplete={c => setColor(c.hex)}
+					colors={[
+						'#FFFFFF', '#FFFF66', '#FFCC66', '#FF6666', '#FF66FF', '#CC66FF',
+						'#66CCFF', '#99FFFF', '#66FFCC', '#66FF66', '#6666FF', '#6666CC',
+						
+						'#CCCCCC', '#FFFF00', '#FFAA00', '#FF3333', '#FF00FF', '#AA00FF',
+						'#00AAFF', '#33FFFF', '#00FFAA', '#00FF33', '#3333FF', '#3333AA',
+					]}
 				/>
 			</Popup>
 		</div>
