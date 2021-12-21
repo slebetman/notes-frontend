05b4dfb651568455bd5a1261d146e1e2e5d11c7e
slebetman <slebetman@gmail.com>
2021-12-21 12:52:59 +0800

    Automatically make urls into links

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import { CompactPicker } from 'react-color';
 import Popup from "reactjs-popup";
+import Linkify from "react-linkify/dist/components/Linkify";
 const notes = require('../api/notes');
 
 function Notes () {
@@ -9,6 +10,7 @@ function Notes () {
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
 	const [color, setColor] = useState('#FFFFFF');
+	const [editing, setEditing] = useState(false);
 
 	useEffect(async () => {
 		if (id !== 'new') {
@@ -17,6 +19,9 @@ function Notes () {
 			setTitle(note.note.title);
 			setContent(note.note.content);
 			setColor(note.note.color);
+		}
+		else {
+			setEditing(true);
 		}
 	},[]);
 
@@ -27,57 +32,95 @@ function Notes () {
 		else {
 			await notes.editNote(id, title, content, color);
 		}
+		setEditing(false);
 	}
 
 	return <div id="note">
 		<div className="input-group">
-			<input type="text" id="title"
-				value={title}
-				onChange={(e) => {
-					setTitle(e.target.value);
-				}}
-			/>
-			<Popup trigger={<button id="color">Color</button>} position="bottom right">
-				<CompactPicker
-					color={color}
-					onChangeComplete={c => setColor(c.hex)}
-					colors={[
-						'#FFFFFF', '#FFFF66', '#FFCC66', '#FF6666', '#FF66FF', '#CC66FF',
-						'#66CCFF', '#99FFFF', '#66FFCC', '#66FF66', '#6666FF', '#6666CC',
-						
-						'#CCCCCC', '#FFFF00', '#FFAA00', '#FF3333', '#FF00FF', '#AA00FF',
-						'#00AAFF', '#33FFFF', '#00FFAA', '#00FF33', '#3333FF', '#3333AA',
-					]}
+			{ editing ?
+				<>
+					<input type="text" id="title"
+						value={title}
+						onChange={(e) => {
+							setTitle(e.target.value);
+						}}
+					/>
+					<Popup trigger={<button id="color">Color</button>} position="bottom right">
+						<CompactPicker
+							color={color}
+							onChangeComplete={c => setColor(c.hex)}
+							colors={[
+								'#FFFFFF', '#FFFF66', '#FFCC66', '#FF6666', '#FF66FF', '#CC66FF',
+								'#66CCFF', '#99FFFF', '#66FFCC', '#66FF66', '#6666FF', '#6666CC',
+								
+								'#CCCCCC', '#FFFF00', '#FFAA00', '#FF3333', '#FF00FF', '#AA00FF',
+								'#00AAFF', '#33FFFF', '#00FFAA', '#00FF33', '#3333FF', '#3333AA',
+							]}
+						/>
+					</Popup>
+				</>
+				:
+				<h3>{title}</h3>
+			}
+		</div>
+		<div className="input-group">
+			{ editing ?
+				<textarea id="body"
+					style={{ backgroundColor: color }}
+					value={content} 
+					onChange={(e) => {
+						setContent(e.target.value);
+					}}
 				/>
-			</Popup>
+				:
+				<div id="body"
+					style={{ backgroundColor: color }}
+					onClick={() => setEditing(true)}
+					onTouchStart={() => setEditing(true)}
+				>
+					<Linkify properties={{target: '_blank'}}>
+						{content}
+					</Linkify>
+				</div>
+			}
 		</div>
 		<div className="input-group">
-			<textarea id="body"
-				style={{ backgroundColor: color }}
-				value={content} 
-				onChange={(e) => {
-					setContent(e.target.value);
-				}}
-			/>
-		</div>
-		<div className="input-group">
-			<button
-				onClick={async () => {
-					await saveNote();
-					console.log('saved');
+			{ editing ?
+				<>
+					<button
+						onClick={async () => {
+							await saveNote();
+							console.log('saved');
 
-					window.location.href = '/';
-				}}
-			>
-				Save
-			</button>
-			<button
-				onClick={() => {
-					window.location.href = '/';
-				}}
-			>
-				Cancel
-			</button>
+							setEditing(false);
+						}}
+					>
+						Save
+					</button>
+					<button
+						onClick={() => {
+							if (id === 'new') {
+								window.location.href = '/';
+							}
+							else {
+								setEditing(false);
+							}
+						}}
+					>
+						Cancel
+					</button>
+				</>
+				:
+				<>
+					<button
+						onClick={() => {
+							window.location.href = '/';
+						}}
+					>
+						Back
+					</button>
+				</>
+			}
 		</div>
 	</div>;
 }
